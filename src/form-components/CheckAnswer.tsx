99db57a290e290bb67da9ac1f7dda865c8cbcd89
Alex Maneri <amaneri@udel.edu>
2023-10-06 10:51:31 -0400

    final mc fix?

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateDescription(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="AnswerChecker">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={3}
+                    value={answer}
+                    onChange={updateDescription}
+                />
+            </Form.Group>
+            answer === {expectedAnswer} ? <div>✔️</div>: <div>❌</div>;
         </div>
     );
 }
